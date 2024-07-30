4d5f57b1b01f12a05fd1e59a2dcdd32af7347c79
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2024-07-30 22:16:02 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ repositories {
 dependencies {
     compileOnly("ink.ptms.core:v12004:12004:mapped")
     compileOnly("ink.ptms.core:v12004:12004:universal")
-    implementation(kotlin("stdlib"))
+    compileOnly(kotlin("stdlib"))
     compileOnly(fileTree("libs"))
 }
 
