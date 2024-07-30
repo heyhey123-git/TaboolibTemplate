a86412920e8c5260d836f323e49d9a2fa4a37bfa
heyhey123-git <156066831+heyhey123-git@users.noreply.github.com>
2024-07-30 21:40:38 +0900

    初始化

---
+++ build.gradle.kts
@@ -0,0 +1,64 @@
+import io.izzel.taboolib.gradle.BUKKIT
+import io.izzel.taboolib.gradle.UNIVERSAL
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    java
+    id("io.izzel.taboolib") version "2.0.11"
+    id("org.jetbrains.kotlin.jvm") version "1.9.22"
+}
+
+taboolib {
+    env {
+        forceDownloadInDev = true
+        // 安装模块
+        install(UNIVERSAL, BUKKIT)
+    }
+    version {
+        taboolib = "6.1.2-beta10"
+        // 跳过 Kotlin 加载
+        skipKotlin = false
+        // 跳过 Kotlin 重定向
+        skipKotlinRelocate = false
+        // 跳过 TabooLib 重定向
+        skipTabooLibRelocate = false
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("ink.ptms.core:v12004:12004:mapped")
+    compileOnly("ink.ptms.core:v12004:12004:universal")
+    implementation(kotlin("stdlib"))
+    compileOnly(fileTree("libs"))
+}
+
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            languageVersion = "2.0"
+        }
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs = listOf("-Xjvm-default=all")
+    }
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+configure<JavaPluginExtension> {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
